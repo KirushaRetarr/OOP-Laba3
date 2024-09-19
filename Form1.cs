38a551eac4df0c7dcdfc12cbf2ed2addfb1e8116
Kirill Durnin <kirill2308@mail.ru>
2024-09-19 06:28:51 +0500

    1-zadanie

--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,29 @@ namespace OOP_Laba3
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double x = double.Parse(textBox1.Text);
+            double y = double.Parse(textBox2.Text);
+
+            if ((y != 0) && (Math.Abs(x) > Math.Abs(y)))
+            {
+                textBox3.Text = (1 / Math.Atan(x / y)).ToString();
+            }
+            else
+            {
+                textBox3.Text = 0.ToString();
+            }
+
+            if ((y != 0) && (Math.Abs(x) <= Math.Abs(y)))
+            {
+                textBox4.Text = (1 / Math.Asin(x / y)).ToString();
+            }
+            else
+            {
+                textBox4.Text = 0.ToString();
+            }
+        }
     }
 }
